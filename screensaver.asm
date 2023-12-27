20c76126fdf0448068e53f056128975dbf3e6578
Barrett Otte <barrettotte@gmail.com>
2023-12-27 16:05:38 -0500

    move square (206 bytes)

--- screensaver.asm
+++ screensaver.asm
@@ -1,18 +1,22 @@
 ;
 
-%define SCREEN_WIDTH 320
-%define SCREEN_HEIGHT 200
-
-%define COLOR_MAGENTA 13
 %define SQUARE_WIDTH 25
 
-%define KEYBOARD_INTERRUPT 0x16
-%define TTY_READ 0x0
+%define SCREEN_WIDTH 320
+%define SCREEN_HEIGHT 200
+%define COLOR_BLACK 0
+%define COLOR_MAGENTA 13
 
 %define VIDEO_INTERRUPT 0x10
 %define VIDEO_TTY_OUT 0x0E
 %define MODE_TEXT 0x3
 %define MODE_VIDEO 0x13
+
+%define MISC_INTERRUPT 0x15
+%define MISC_WAIT 0x86
+
+%define KEYBOARD_INTERRUPT 0x16
+%define TTY_READ 0x0
 
 %define VIDEO_SEGMENT 0xA000
 %define ORIGIN 0x7C00
@@ -32,12 +36,61 @@ draw:                                          ; ***** draw ******
       mov ax, MODE_VIDEO                       ; set video mode to 320x200, 256 colors
       int VIDEO_INTERRUPT                      ; BIOS interrupt
 
+%define x_i 50
+%define y_i 50
+
+      mov cx, 1
+      mov dx, x_i
+draw_loop:
+      cmp cx, 1
+      je square ; don't erase on first frame
+
+erase:
+      push cx
+      push dx
+
       push SQUARE_WIDTH                        ; height
       push SQUARE_WIDTH                        ; width
-      push 100                                 ; x position
-      push 100                                 ; y position
-      push COLOR_MAGENTA                       ; color
+      push dx                                  ; y_0
+      push dx                                  ; x_0
+      push COLOR_BLACK                         ; color 
       call draw_rect                           ; draw rectangle
+      add sp, 2*5                              ; remove args from stack
+
+      pop dx
+      pop cx
+
+square:
+      ; ===== TEMP =====
+      mov dx, cx
+      shl dx, 3
+      add dx, x_i
+      ; ================
+
+      push cx
+      push dx
+
+      push SQUARE_WIDTH                        ; height
+      push SQUARE_WIDTH                        ; width
+      push dx                                  ; y_0
+      push dx                                  ; x_0
+      push cx                                  ; color 
+      call draw_rect                           ; draw rectangle
+      add sp, 2*5                              ; remove args from stack
+
+      ; mov cx, 18                               ; NOTE: This interrupt seems to behave strangely in emulated environments.
+      ; mov dx, 500                              ;   I just plugged in arbitrary numbers in here until I got 1s delay. 
+      mov cx, 3
+      mov dx, 500
+      mov ah, MISC_WAIT                        ; wait (for 1 second)
+      int MISC_INTERRUPT                       ; BIOS interrupt
+
+      pop dx
+      pop cx
+draw_next:
+      inc cx                                   ; i++
+      cmp cx, 8                                ;
+      jl draw_loop                             ; continue drawing
 
 wait_0:                                        ; ***** wait for keypress *****
       mov ah, TTY_READ                         ; read character
@@ -84,7 +137,7 @@ draw_rect:                                     ; ***** draw rectangle *****
       inc bx                                   ; x++
       mov ax, [bp+6]                           ; x_0
       add ax, [bp+10]                          ; x_0 + width
-      cmp bl, al                               ; check column loop condition
+      cmp bx, ax                               ; check column loop condition
       jl .draw_col                             ; while x < x_0 + width
 .next_row:                                     ; iterate to next row
       inc cx                                   ; y++
