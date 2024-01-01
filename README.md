4c980288ea5638ac2375dbe055b582fad7c77156
Barrett Otte <barrettotte@gmail.com>
2024-01-01 15:44:44 -0500

    removes prompt loop (205 bytes)

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ A bouncing square screensaver-like thing in x86 real mode.
 ## References
 
 - https://en.wikipedia.org/wiki/BIOS_interrupt_call
+- https://en.wikipedia.org/wiki/Mode_13h
 - [PC Interrupts: A Programmer's Reference to BIOS, DOS, and Third Party Calls](https://isbnsearch.org/isbn/9780201577976)
--- screensaver.asm
+++ screensaver.asm
@@ -1,22 +1,24 @@
 ; A bouncing square screensaver-like thing
 
+%define PX_0 10
+%define PY_0 10
+%define VX_0 1
+%define VY_0 1
+%define SPEED 2
 %define SQUARE_WIDTH 25
 
 %define SCREEN_WIDTH 320
 %define SCREEN_HEIGHT 200
 %define COLOR_BLACK 0
 %define COLOR_MAGENTA 13
+%define COLOR_RED 4
 
 %define VIDEO_INTERRUPT 0x10
 %define VIDEO_TTY_OUT 0x0E
-%define MODE_TEXT 0x3
 %define MODE_VIDEO 0x13
 
 %define MISC_INTERRUPT 0x15
 %define MISC_WAIT 0x86
-
-%define KEYBOARD_INTERRUPT 0x16
-%define TTY_READ 0x0
 
 %define VIDEO_SEGMENT 0xA000
 %define ORIGIN 0x7C00
@@ -24,106 +26,103 @@
       org ORIGIN                               ; set boot sector origin
       cpu 386                                  ;
       bits 16                                  ;
-
 _start:                                        ; ***** program entry *****
 
 main:                                          ; ***** main program loop *****
-      xor ax, ax                               ; AX = 0
+      xor ax, ax                               ;
       mov ds, ax                               ; reset data segment
       mov es, ax                               ; reset extra segment
 
-draw:                                          ; ***** draw ******
       mov ax, MODE_VIDEO                       ; set video mode to 320x200, 256 colors
       int VIDEO_INTERRUPT                      ; BIOS interrupt
+      mov ax, VIDEO_SEGMENT                    ; graphics video memory segment
+      mov es, ax                               ; set extra segment to video memory
 
-; ===== TEMP =====
-%define x_0 25
-%define y_0 25
-; ================
-
-      mov word [curr_x], x_0
-      mov word [curr_y], y_0
-      mov cx, 1
-draw_loop:
-      push cx
-      cmp cx, 1
+      mov cx, 1                                ; i TODO: remove all refs to i
+draw_loop:                                     ; ***** draw a frame *****
+      push cx                                  ;
+      cmp cx, 1                                ; 
       je square                                ; don't erase on first frame
+      ; TODO: might not need this check when velocity add moved
 
-erase:
-      mov ax, [curr_x]
-      mov bx, [curr_y]
-
-      push SQUARE_WIDTH                        ; height
-      push SQUARE_WIDTH                        ; width
-      push bx                                  ; y = curr_y
-      push ax                                  ; x = curr_x
-      push COLOR_BLACK                         ; color 
+erase_prev:                                    ; ***** clear previous square *****
+      push SQUARE_WIDTH                        ; arg: height
+      push SQUARE_WIDTH                        ; arg: width
+      mov bx, [py]                             ; load current y position
+      push bx                                  ; arg: y
+      mov ax, [px]                             ; load current x position
+      push ax                                  ; arg: x
+      push COLOR_BLACK                         ; arg: color
       call draw_rect                           ; draw rectangle
       add sp, 2*5                              ; remove args from stack
 
-square:
-      mov dx, [curr_x]                         ; load current x
-      add dx, 8                                ; move x direction
-      mov [curr_x], dx                         ; save current x
+square:                                        ; draw square
+      mov ax, [px]                             ; load current x position
+      add ax, SPEED                            ; move x direction
+      mov [px], ax                             ; save current x position
 
-      mov dx, [curr_y]                         ; load current y
-      add dx, 8                                ; move y direction
-      mov [curr_y], dx                         ; save current y
+      mov ax, [py]                             ; load current y position
+      add ax, SPEED                            ; move y direction
+      mov [py], ax                             ; save current y position
 
       ; TODO: change direction when screen bounds hit
 
-      mov ax, [curr_color]                     ; load current color
-      inc ax                                   ; change color
-      cmp ax, 8                                ; check if color wrap needed
-      jle skip_color_wrap                      ; skip color wrap if not needed
-      mov ax, 1                                ; wrap colors around
-skip_color_wrap:
-      mov [curr_color], ax                     ; update current color
+;       inc ax                                   ; change color
+;       cmp ax, 8                                ; check if color wrap needed
+;       jle skip_color_wrap                      ; skip color wrap if not needed
+;       mov ax, 1                                ; wrap colors around
+; skip_color_wrap:
+;       mov [color], ax                          ; update current color
 
-      push SQUARE_WIDTH                        ; height
-      push SQUARE_WIDTH                        ; width
-      mov dx, [curr_y]                         ; load current y
-      push dx                                  ; y position
-      mov dx, [curr_x]                         ; load current x
-      push dx                                  ; x position
-      push ax                                  ; color 
+      push SQUARE_WIDTH                        ; arg: height
+      push SQUARE_WIDTH                        ; arg: width
+      mov ax, [py]                             ; load current y position
+      push ax                                  ; arg: y
+      mov ax, [px]                             ; load current x position
+      push ax                                  ; arg: x
+      mov ax, [color]                          ; load current color
+      push ax                                  ; arg: color
       call draw_rect                           ; draw rectangle
       add sp, 2*5                              ; remove args from stack
 
-      mov cx, 3
-      mov dx, 500
+      mov cx, 1                                ;
+      mov dx, 100                              ;
       ; mov cx, 18                               ; NOTE: This interrupt seems to behave strangely in emulated environments.
       ; mov dx, 500                              ;   I just plugged in arbitrary numbers in here until I got 1s delay. 
-      mov ah, MISC_WAIT                        ; wait (for 1 second)
+      mov ah, MISC_WAIT                        ; wait
       int MISC_INTERRUPT                       ; BIOS interrupt
 
-draw_next:
+      mov cx, [px]                             ; load current x position
+      mov dx, [py]                             ; load current y position
+check_top:                                     ; check top bound
+      cmp dx, 0                                ;
+      jle do_bounce                            ; bounce if top bound hit (py <= 0)
+check_left:                                    ; check left bound
+      cmp cx, 0                                ;
+      jle do_bounce                            ; bounce if left bound hit (px <= 0)
+check_right:                                   ; check right bound
+      cmp cx, SCREEN_WIDTH-SQUARE_WIDTH        ;
+      jge do_bounce                            ; bounce if right bound hit (px >= SCREEN_WIDTH-SQUARE_WIDTH)
+check_bottom:                                  ; check bottom bound
+      cmp dx, SCREEN_HEIGHT-SQUARE_WIDTH       ;
+      jge do_bounce                            ; bounce if bottom bound hit (py >= SCREEN_HEIGHT-SQUARE_WIDTH)
+no_bounce:                                     ; no bounds hit
+      jmp draw_next                            ; skip bouncing
+
+do_bounce:                                     ; change direction depending on wall hit
+      ; TODO:
+      nop
+      ; if left or right bound hit, reverse x velocity
+      ; if top or bottom bound hit, reverse y velocity
+      ; x++ if x == 1, else x--
+      ; y++ if y == 1, else y--
+
+draw_next:                                     ; continue to next iteration
+      ; TODO: remove loop end and refs to i
       pop cx                                   ; restore i
       inc cx                                   ; i++
-      cmp cx, 16+1                             ; check loop condition
+      cmp cx, 60+1                             ; check loop condition
       jl draw_loop                             ; continue drawing
-
-wait_0:                                        ; ***** wait for keypress *****
-      mov ah, TTY_READ                         ; read character
-      int KEYBOARD_INTERRUPT                   ; BIOS interrupt
-      mov ax, MODE_TEXT                        ; set video mode to text mode, 80x25
-      int VIDEO_INTERRUPT                      ; BIOS interrupt
-
-print_prompt:                                  ; ***** print prompt to console *****
-      mov si, prompt                           ; load pointer to prompt
-prompt_loop:                                   ;
-      lodsb                                    ; AL = prompt[SI]
-      cmp al, 0                                ; check for null terminator
-      je reset                                 ; prompt finished printing
-
-      mov ah, VIDEO_TTY_OUT                    ; teletype output
-      int VIDEO_INTERRUPT                      ; BIOS interrupt
-      jmp prompt_loop                          ; continue printing
-
-reset:                                         ; ***** reset to beginning *****
-      mov ah, TTY_READ                         ; read character
-      int KEYBOARD_INTERRUPT                   ; BIOS interrupt
-      jmp main                                 ; loop to beginning
 
 end:                                           ; ***** end of program *****
       jmp $                                    ; infinite loop
@@ -138,11 +137,11 @@ draw_rect:                                     ; ***** draw rectangle *****
       push bp                                  ; save base pointer
       mov bp, sp                               ; setup stack frame
 
-      mov cx, [bp+8]                           ; y = y_0
+      mov cx, [bp+8]                           ; arg: y = y_0
 .draw_row:                                     ; row loop
-      mov bx, [bp+6]                           ; x = x_0
+      mov bx, [bp+6]                           ; arg: x = x_0
 .draw_col:                                     ; column loop
-      mov al, [bp+4]                           ; pixel color
+      mov al, [bp+4]                           ; arg: pixel color
       call set_pixel                           ; write pixel to screen
 .next_col:                                     ; iterate to next column
       inc bx                                   ; x++
@@ -166,10 +165,7 @@ set_pixel:                                     ; ***** write a pixel to screen *
                                                ; input BX - x coordinate
                                                ; input CX - y coordinate
       push di                                  ;
-      push ax                                  ; store pixel color
-
-      mov ax, VIDEO_SEGMENT                    ; graphics video memory segment
-      mov es, ax                               ; set extra segment to video memory
+      push ax                                  ; save pixel color
 
       mov ax, SCREEN_WIDTH                     ; offset = SCREEN_WIDTH
       mul cx                                   ; offset = (y * SCREEN_WIDTH)
@@ -183,10 +179,12 @@ set_pixel:                                     ; ***** write a pixel to screen *
       pop di                                   ;
       ret                                      ; end set_pixel subroutine
 
-prompt: db 'Press any key to continue...', 0   ; prompt to reset
-curr_x: dw 0                                   ; current x coordinate
-curr_y: dw 0                                   ; current y coordinate
-curr_color: db 0                               ; current color of square
+px:   dw PX_0                                  ; x position
+py:   dw PY_0                                  ; y position
+vx:   db VX_0                                  ; x velocity (1=right, 0=left)
+vy:   db VY_0                                  ; y velocity (1=down, 0=up)
+color:                                         ;
+      db COLOR_RED                             ; color of square (init red)
 
 %ifdef skip_fill
 %else
